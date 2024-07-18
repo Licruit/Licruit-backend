fa54dd7dbae7bb8c5dbe3e7d4257e4daa8327338
ncherryu <alswls8345@naver.com>
2024-07-18 16:58:05 +0900

    feat: 유효성 검사 기본 설정 생성

---
+++ validators/validate.js
@@ -0,0 +1,11 @@
+const { validationResult } = require('express-validator');
+
+exports.validate = (req, res, next) => {
+    const err = validationResult(req);
+
+    if (err.isEmpty()) {
+        return next();
+    } else {
+        return res.status(400).json(err.array());
+    }
+}
