b8099fd7d579ae20134a63ade7a51e2206330286
ncherryu <alswls8345@naver.com>
2024-07-23 12:27:08 +0900

    style: 들여쓰기 수정

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,23 +3,23 @@ const authService = require("../services/users.service");
 const { insertUser } = require('../services/users.service');
 
 exports.postOtp = async (req, res, next) => {
-  try {
-    const { phoneNumber } = req.body;
-    await authService.sendOtp(phoneNumber);
-    return res.status(StatusCodes.OK).json({ message: "인증번호 전송에 성공했습니다."});
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const { phoneNumber } = req.body;
+        await authService.sendOtp(phoneNumber);
+        return res.status(StatusCodes.OK).json({ message: "인증번호 전송에 성공했습니다." });
+    } catch (err) {
+        next(err);
+    }
 };
 
 exports.verifyOtp = async (req, res, next) => {
-  try {
-    const { phoneNumber, code } = req.body;
-    await authService.verifyOtp(phoneNumber, code);
-    return res.status(StatusCodes.OK).json({ message : "인증에 성공했습니다."});
-  } catch (err) {
-    next(err)
-  }
+    try {
+        const { phoneNumber, code } = req.body;
+        await authService.verifyOtp(phoneNumber, code);
+        return res.status(StatusCodes.OK).json({ message: "인증에 성공했습니다." });
+    } catch (err) {
+        next(err)
+    }
 };
 
 exports.addUser = async (req, res, next) => {
