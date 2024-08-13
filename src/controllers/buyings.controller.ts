c731d308778df06f02c957fb9d568cbf13e161eb
ncherryu <alswls8345@naver.com>
2024-08-13 18:51:06 +0900

    refactor: 공동구매 목록 조회 예외 추가

--- src/controllers/buyings.controller.ts
+++ src/controllers/buyings.controller.ts
@@ -2,12 +2,16 @@ import { Request, Response } from 'express';
 import { FilterType } from '../dto/buyings.dto';
 import { selectBuyings } from '../services/buyings.service';
 import { StatusCodes } from 'http-status-codes';
+import HttpException from '../utils/httpExeption';
 
 export const getBuygins = async (req: Request, res: Response) => {
   const filter = req.query.filter as FilterType;
   const page = parseInt(req.query.page as string);
 
   const buyingList = await selectBuyings(filter, page);
+  if (!buyingList.buyings.length) {
+    throw new HttpException(StatusCodes.NOT_FOUND, '조회할 공동구매 목록이 없습니다.');
+  }
 
   return res.status(StatusCodes.OK).json(buyingList);
 };
