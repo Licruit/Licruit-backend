cf73a70b1fe86e71fd4ea786082cae70517e8baa
ncherryu <alswls8345@naver.com>
2024-08-15 18:38:33 +0900

    feat: 참여한 공동구매 상세 조회 API 추가

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -2,7 +2,13 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { TokenRequest } from '../auth';
 import { AllOrdersDTO } from '../dto/orders.dto';
-import { isOrderOwner, selectAllOrders, selectOrderSummary, updateCanceledOrder } from '../services/orders.service';
+import {
+  isOrderOwner,
+  selectAllOrders,
+  selectOrderDetail,
+  selectOrderSummary,
+  updateCanceledOrder,
+} from '../services/orders.service';
 import HttpException from '../utils/httpExeption';
 
 export const getAllOrders = async (req: Request, res: Response) => {
@@ -36,4 +42,17 @@ export const cancelOrder = async (req: Request, res: Response) => {
   await updateCanceledOrder(orderId);
 
   return res.status(StatusCodes.OK).end();
+};
+
+export const getOrderDetail = async (req: Request, res: Response) => {
+  const companyNumber = (req as TokenRequest).token.companyNumber;
+  const orderId = parseInt(req.params.orderId);
+
+  const isOwner = await isOrderOwner(companyNumber, orderId);
+  if (!isOwner) {
+    throw new HttpException(StatusCodes.UNAUTHORIZED, '해당 주문에 접근 권한이 없습니다.');
+  }
+  const orderDetail = await selectOrderDetail(orderId);
+
+  return res.status(StatusCodes.OK).json(orderDetail);
 };
